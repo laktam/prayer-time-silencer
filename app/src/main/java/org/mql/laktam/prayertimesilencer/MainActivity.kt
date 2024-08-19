fc9e7c51f7dc88acc4b669386977eaba8e7f424c
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-19 23:36:50 +0100

    add RequestDNDAccessButton

--- app/src/main/java/org/mql/laktam/prayertimesilencer/MainActivity.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.mql.laktam.prayertimesilencer
 
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,14 +15,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import org.mql.laktam.prayertimesilencer.ui.theme.PrayerTimeSilencerTheme
 
 import android.content.Context
+import android.content.Intent
 import android.media.AudioManager
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.provider.Settings
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import org.mql.laktam.prayertimesilencer.ui.theme.PrayerTimeSilencerTheme
+import androidx.compose.ui.platform.LocalContext
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,6 +66,28 @@ fun SilenceButton(context: Context, modifier: Modifier = Modifier) {
     }
 }
 
+
+@Composable
+fun RequestDNDAccessButton(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    Button(
+        onClick = {
+            if (!notificationManager.isNotificationPolicyAccessGranted) {
+                val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+                context.startActivity(intent)
+            } else {
+                // If permission is already granted, you can silence the phone here.
+                val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+                audioManager.ringerMode = AudioManager.RINGER_MODE_SILENT
+            }
+        },
+        modifier = modifier
+    ) {
+        Text(text = "Silence Phone")
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
