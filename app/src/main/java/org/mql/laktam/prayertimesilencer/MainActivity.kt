28c945ace9a87ef77f2db226df5e5523a7b368b6
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-19 22:52:20 +0100

    add permission in androidManifest to change ringer mode

--- app/src/main/java/org/mql/laktam/prayertimesilencer/MainActivity.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PrayerTimeSilencerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Silence",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
