959d59644992e145dcc457ed3e7db99395818470
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-21 16:35:41 +0100

    add ActivationButton to launch and stop service

--- app/src/main/java/org/mql/laktam/prayertimesilencer/ActivationButton.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/ActivationButton.kt
@@ -1,21 +1,65 @@
 package org.mql.laktam.prayertimesilencer
 
+import android.Manifest
+import android.app.Activity
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.provider.Settings
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
 
 @Composable
-fun ActivationButton() {
+fun ActivationButton(modifier: Modifier = Modifier) {
     val context = LocalContext.current
+
+
+    // permissions check
+    var locationPermissionGranted by remember { mutableStateOf(false) }
+    var doNotDisturbPermissionGranted by remember { mutableStateOf(false) }
+
+    if (!locationPermissionGranted) {
+        locationPermissionGranted = ActivityCompat.checkSelfPermission(
+            context, Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (!locationPermissionGranted) {
+            LaunchedEffect(Unit) {
+                ActivityCompat.requestPermissions(
+                    context as Activity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    1
+                )
+            }
+        }
+    }
+
+    // Check and request Do Not Disturb permission
+    if (!doNotDisturbPermissionGranted) {
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        doNotDisturbPermissionGranted = notificationManager.isNotificationPolicyAccessGranted
+
+        if (!doNotDisturbPermissionGranted) {
+            LaunchedEffect(Unit) {
+                val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+                context.startActivity(intent)
+            }
+        }
+    }
+
     var isServiceRunning by remember { mutableStateOf(false) }
 
     val handleButtonClick = {
@@ -23,25 +67,28 @@ fun ActivationButton() {
             stopService(context)
             isServiceRunning = false
         } else {
-            startService(context)
+            startService(context, locationPermissionGranted, doNotDisturbPermissionGranted)
             isServiceRunning = true
         }
     }
-
     Column {
-        Button(onClick = { handleButtonClick() }) {
+        Button(onClick = { handleButtonClick() }, modifier = modifier) {
             Text(if (isServiceRunning) "Stop Service" else "Start Service")
         }
     }
 }
 
 
-fun startService(context: Context) {
-    val serviceIntent = Intent(context, MyForegroundService::class.java)
-    ContextCompat.startForegroundService(context, serviceIntent)
+fun startService(context: Context, locationPermissionGranted: Boolean, doNotDisturbPermissionGranted : Boolean) {
+    if (locationPermissionGranted && doNotDisturbPermissionGranted) {
+        val serviceIntent = Intent(context, PrayerTimeService::class.java)
+        ContextCompat.startForegroundService(context, serviceIntent)
+    }
+
 }
 
 fun stopService(context: Context) {
-    val serviceIntent = Intent(context, MyForegroundService::class.java)
+    val serviceIntent = Intent(context, PrayerTimeService::class.java)
     context.stopService(serviceIntent)
+
 }
