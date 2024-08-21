6014028d4a1ee4bd08498440eab0c82a999fc201
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-21 16:35:58 +0100

    add PrayerTimeService

--- app/src/main/java/org/mql/laktam/prayertimesilencer/MainActivity.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/MainActivity.kt
@@ -55,15 +55,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             PrayerTimeSilencerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SilenceButton(
-                        modifier = Modifier.padding(innerPadding),
-                        fusedLocationClient = fusedLocationClient
-                    )
+//                    SilenceButton(
+//                        modifier = Modifier.padding(innerPadding),
+//                        fusedLocationClient = fusedLocationClient
+//                    )
+                    ActivationButton(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun SilenceButton(modifier: Modifier = Modifier, fusedLocationClient: FusedLocationProviderClient) {
@@ -106,181 +108,75 @@ fun SilenceButton(modifier: Modifier = Modifier, fusedLocationClient: FusedLocat
     // Button to silence the phone based on location
     Button(
         onClick = {
-            if (locationPermissionGranted && doNotDisturbPermissionGranted) {
-                fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                    if (location != null) {
-                        val latitude = location.latitude
-                        val longitude = location.longitude
-                        println("Latitude: $latitude, Longitude: $longitude")
-//
-//                        // Perform the API call and silence the phone
-                        coroutineScope.launch {
-                            try {
-                                val response = RetrofitInstance.api.getPrayerTimes(latitude, longitude)
-                                val fajrTime = response.data.timings.Fajr
-                                println("Fajr time: $fajrTime")
-                                // Use the fetched prayer time
-//                                val fajrDate =  stringToDate(fajrTime);
-                                val fajrDate =  getPrayerTime("19:47");
-                                if(fajrDate != null){
-                                    schedulePhoneSilence(context, fajrDate)
-                                }
-//                                schedulePhoneSilence(context, fajrTime)
-                            } catch (e: Exception) {
-                                e.printStackTrace()
-                                println("Failed to fetch prayer times")
-                            }
-                        }
-//                        (context as Activity).lifecycleScope.launch {
-//                            val prayerTime = fetchPrayerTime(latitude, longitude)
-//                            if (prayerTime != null) {
-//                                schedulePhoneSilence(context, prayerTime)
+//            if (locationPermissionGranted && doNotDisturbPermissionGranted) {
+//                fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+//                    if (location != null) {
+//                        val latitude = location.latitude
+//                        val longitude = location.longitude
+//                        println("Latitude: $latitude, Longitude: $longitude")
+////
+////                        // Perform the API call and silence the phone
+//                        coroutineScope.launch {
+//                            try {
+//                                val response = RetrofitInstance.api.getPrayerTimes(latitude, longitude)
+//                                val fajrTime = response.data.timings.Fajr
+//                                println("Fajr time: $fajrTime")
+//                                // Use the fetched prayer time
+////                                val fajrDate =  stringToDate(fajrTime);
+//                                val fajrDate =  getPrayerTime("19:47");
+//                                if(fajrDate != null){
+//                                    schedulePhoneSilence(context, fajrDate)
+//                                }
+////                                schedulePhoneSilence(context, fajrTime)
+//                            } catch (e: Exception) {
+//                                e.printStackTrace()
+//                                println("Failed to fetch prayer times")
 //                            }
 //                        }
-                    } else {
-                        println("Location not available")
-                    }
-                }
-            } else {
-                println("Permissions not granted")
-            }
+//                    } else {
+//                        println("Location not available")
+//                    }
+//                }
+//            } else {
+//                println("Permissions not granted")
+//            }
         },
         modifier = modifier
     ) {
         Text(text = "Silence Phone")
     }
 
-//
-//    // Request location permission if not granted
-//    val locationPermissionState = remember {
-//        // Check and request location permission
-//        ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-//    }
-//
-//    if (!locationPermissionState) {
-//        // Request permission
-//        LaunchedEffect(Unit) {
-//            ActivityCompat.requestPermissions(
-//                context as ComponentActivity,
-//                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-//                1
-//            )
-//        }
-//    }
-//
-//    Button(
-//        onClick = {
-//            // Check if the permission is granted
-//            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-//                fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-//                    if (location != null) {
-//                        val latitude = location.latitude
-//                        val longitude = location.longitude
-//                        // Use latitude and longitude as needed
-//                        println("Latitude: $latitude, Longitude: $longitude")
-//                    } else {
-//                        println("Location not available")
-//                    }
-//                }
-//            } else {
-//                // Request Do Not Disturb access if not granted
-//                val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
-//                context.startActivity(intent)
-//            }
-//        },
-//        modifier = modifier
-//    ) {
-//        Text(text = "Silence Phone")
-//    }
 }
 
-// Function to fetch prayer time from the API
-//suspend fun fetchPrayerTime(latitude: Double, longitude: Double): Date? {
-//    try {
-//        val apiUrl = "https://api.aladhan.com/v1/timings?latitude=$latitude&longitude=$longitude"
-//        val response = URL(apiUrl).readText()
+
 //
-//        // Parsing the prayer time from the response
-//        val prayerTimeString = parsePrayerTimeFromResponse(response)
+//fun getPrayerTime(timeString: String): Date? {
+//    return try {
+//        // Parse the time string "HH:mm"
+//        val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+//        val parsedTime = timeFormat.parse(timeString)
 //
-//        return SimpleDateFormat("HH:mm", Locale.getDefault()).parse(prayerTimeString)
+//        // Combine the parsed time with the current date
+//        val calendar = Calendar.getInstance()
+//        val currentDate = calendar.time
+//
+//        if (parsedTime != null) {
+//            calendar.time = parsedTime
+//            val hours = calendar.get(Calendar.HOUR_OF_DAY)
+//            val minutes = calendar.get(Calendar.MINUTE)
+//
+//            calendar.time = currentDate
+//            calendar.set(Calendar.HOUR_OF_DAY, hours)
+//            calendar.set(Calendar.MINUTE, minutes)
+//            calendar.set(Calendar.SECOND, 0)
+//            calendar.set(Calendar.MILLISECOND, 0)
+//
+//            return calendar.time
+//        } else {
+//            null
+//        }
 //    } catch (e: Exception) {
 //        e.printStackTrace()
-//        return null
-//    }
-//}
-
-fun getPrayerTime(timeString: String): Date? {
-    return try {
-        // Parse the time string "HH:mm"
-        val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
-        val parsedTime = timeFormat.parse(timeString)
-
-        // Combine the parsed time with the current date
-        val calendar = Calendar.getInstance()
-        val currentDate = calendar.time
-
-        if (parsedTime != null) {
-            calendar.time = parsedTime
-            val hours = calendar.get(Calendar.HOUR_OF_DAY)
-            val minutes = calendar.get(Calendar.MINUTE)
-
-            calendar.time = currentDate
-            calendar.set(Calendar.HOUR_OF_DAY, hours)
-            calendar.set(Calendar.MINUTE, minutes)
-            calendar.set(Calendar.SECOND, 0)
-            calendar.set(Calendar.MILLISECOND, 0)
-
-            return calendar.time
-        } else {
-            null
-        }
-    } catch (e: Exception) {
-        e.printStackTrace()
-        null
-    }
-}
-// Function to schedule the phone to be silenced for 25 minutes
-fun schedulePhoneSilence(context: Context, prayerTime: Date) {
-    val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-    val currentTime = Calendar.getInstance().time
-    println("prayer time $prayerTime.time")
-    val startDelay = prayerTime.time - currentTime.time
-    val endDelay = startDelay + 2 * 60 * 1000  // 2 minutes in milliseconds
-// Schedule the phone to be silenced at the start time
-    if (startDelay > 0) {
-        Timer().schedule(timerTask {
-            audioManager.ringerMode = AudioManager.RINGER_MODE_SILENT
-            println("Phone silenced at $prayerTime")
-
-            // Schedule the phone to return to normal mode at the end time
-            Timer().schedule(timerTask {
-                audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
-                println("Phone restored to normal mode 2 minutes after $prayerTime")
-            }, endDelay)
-        }, startDelay)
-    } else {
-        println("Scheduled prayer time has already passed.")
-    }
-//    calendar.add(Calendar.MINUTE, 2)
-//    val endSilence = calendar.time
-//
-//    // Schedule the phone to be silenced at the start time
-//    Timer().schedule(timerTask {
-//        audioManager.ringerMode = AudioManager.RINGER_MODE_SILENT
-//        println("Phone silenced at $startSilence")
-//
-//        // Schedule the phone to return to normal mode at the end time
-//        Timer().schedule(timerTask {
-//            audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
-//            println("Phone restored to normal mode at $endSilence")
-//        }, endSilence)
-//    }, startSilence)
-}
-//@Preview(showBackground = true)
-//@Composable
-//fun SilenceButtonPreview() {
-//    PrayerTimeSilencerTheme {
-//        SilenceButton(fusedLocationClient = FusedLocationProviderClient(LocalContext.current))
+//        null
 //    }
 //}
--- app/src/main/java/org/mql/laktam/prayertimesilencer/PrayerTimeService.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/PrayerTimeService.kt
@@ -1,28 +1,21 @@
 package org.mql.laktam.prayertimesilencer
 
-import android.Manifest
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.location.Location
 import android.media.AudioManager
+import android.os.Build
 import android.os.IBinder
-import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
@@ -30,23 +23,43 @@ import java.util.Locale
 import java.util.Timer
 import kotlin.concurrent.timerTask
 
-class MyForegroundService : Service() {
+
+class PrayerTimeService : Service() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
-
+    private val CHANNEL_ID = "PrayerTimeServiceChannel"
+    private val NOTIFICATION_ID = 1
     override fun onCreate() {
         super.onCreate()
         // Initialize service
+//            val CHANNEL_ID = "my_channel_01"
+//            val channel = NotificationChannel(
+//                CHANNEL_ID,
+//                "Channel human readable title",
+//                NotificationManager.IMPORTANCE_DEFAULT
+//            )
+//
+//            (getSystemService(NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(
+//                channel
+//            )
+//
+//            val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+//                .setContentTitle("")
+//                .setContentText("").build()
+//
+//            startForeground(1, notification)
+        createNotificationChannel()
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         println("service started")
+        startForegroundService()  // Start the service as a foreground service
         getLocation()
         return START_STICKY
     }
 
-    // the permission is checked in activationbutton before launching the service
+    // the permission is checked in ActivationButton before launching the service
     @SuppressLint("MissingPermission")
     private fun getLocation() {
         fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
@@ -63,6 +76,7 @@ class MyForegroundService : Service() {
                 val prayerTimesResponse = prayerTimeApi.getPrayerTimes(latitude, longitude)
                 val prayerTimes = prayerTimesResponse.data.timings
                 println("prayer times $prayerTimes")
+                schedulePrayerTimeSilence(this@PrayerTimeService, prayerTimes)
             } catch (e: Exception) {
                 // Handle the error
             }
@@ -77,43 +91,129 @@ class MyForegroundService : Service() {
         super.onDestroy()
         // Clean up resources
     }
+    private fun schedulePrayerTimeSilence(context: Context, prayerTimes: Timings) {
+//        val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+//        val fajrTime = parseTime(prayerTimes.Fajr)
+//        val dhuhrTime = parseTime(prayerTimes.Dhuhr)
+//        val asrTime = parseTime(prayerTimes.Asr)
+        val fajrTime = parseTime("16:07")
+//        val dhuhrTime = parseTime("15:52")
+//        val asrTime = parseTime("15:58")
+        val maghribTime = parseTime(prayerTimes.Maghrib)
+        val ishaTime = parseTime(prayerTimes.Isha)
 
+        schedulePhoneSilence(context, fajrTime)
+//        schedulePhoneSilence(context, dhuhrTime)
+//        schedulePhoneSilence(context, asrTime)
+        schedulePhoneSilence(context, maghribTime)
+        schedulePhoneSilence(context, ishaTime)
+    }
 
-    fun schedulePhoneSilence(context: Context, prayerTime: Date) {
+    private fun schedulePhoneSilence(context: Context, prayerTime: Date) {
         val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
         val currentTime = Calendar.getInstance().time
-        println("prayer time $prayerTime.time")
         val startDelay = prayerTime.time - currentTime.time
         val endDelay = startDelay + 2 * 60 * 1000  // 2 minutes in milliseconds
-// Schedule the phone to be silenced at the start time
+
         if (startDelay > 0) {
+            // increment delay number
             Timer().schedule(timerTask {
                 audioManager.ringerMode = AudioManager.RINGER_MODE_SILENT
                 println("Phone silenced at $prayerTime")
 
-                // Schedule the phone to return to normal mode at the end time
                 Timer().schedule(timerTask {
                     audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
                     println("Phone restored to normal mode 2 minutes after $prayerTime")
+                    // here after deselencing
+                    // decrement delay number
+                    // test delays number if it is 0
+                    // add delays for the next prayers
                 }, endDelay)
             }, startDelay)
         } else {
             println("Scheduled prayer time has already passed.")
         }
-//    calendar.add(Calendar.MINUTE, 2)
-//    val endSilence = calendar.time
-//
-//    // Schedule the phone to be silenced at the start time
-//    Timer().schedule(timerTask {
-//        audioManager.ringerMode = AudioManager.RINGER_MODE_SILENT
-//        println("Phone silenced at $startSilence")
-//
-//        // Schedule the phone to return to normal mode at the end time
-//        Timer().schedule(timerTask {
-//            audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
-//            println("Phone restored to normal mode at $endSilence")
-//        }, endSilence)
-//    }, startSilence)
     }
+
+//    private fun parseTime(timeString: String): Date {
+//        val formatter = SimpleDateFormat("HH:mm", Locale.getDefault())
+//        return formatter.parse(timeString)!!
+//    }
+    private fun parseTime(timeString: String): Date {
+    return try {
+        // Parse the time string "HH:mm"
+        val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+        val parsedTime = timeFormat.parse(timeString)
+
+        // Combine the parsed time with the current date
+        val calendar = Calendar.getInstance()
+        val currentDate = calendar.time
+
+        if (parsedTime != null) {
+            calendar.time = parsedTime
+            val hours = calendar.get(Calendar.HOUR_OF_DAY)
+            val minutes = calendar.get(Calendar.MINUTE)
+
+            calendar.time = currentDate
+            calendar.set(Calendar.HOUR_OF_DAY, hours)
+            calendar.set(Calendar.MINUTE, minutes)
+            calendar.set(Calendar.SECOND, 0)
+            calendar.set(Calendar.MILLISECOND, 0)
+
+            return calendar.time
+        } else {
+            Calendar.getInstance().time
+        }
+    } catch (e: Exception) {
+        println("##### error in parseTime ")
+        e.printStackTrace()
+        Calendar.getInstance().time
+    }
+
+
+}
+// Required for Android O and above to create a notification channel
+private fun createNotificationChannel() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val serviceChannel = NotificationChannel(
+            CHANNEL_ID,
+            "Prayer Time Service Channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        val manager = getSystemService(NotificationManager::class.java)
+        manager?.createNotificationChannel(serviceChannel)
+    }
+}
+private fun startForegroundService() {
+    val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+        .setContentTitle("Prayer Time Service")
+        .setContentText("Silencing your phone during prayer times.")
+        .setSmallIcon(R.drawable.ic_notification)
+        .build()
+
+    startForeground(NOTIFICATION_ID, notification)
+}
+//    fun schedulePhoneSilence(context: Context, prayerTime: Date) {
+//        val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+//        val currentTime = Calendar.getInstance().time
+//        println("prayer time $prayerTime.time")
+//        val startDelay = prayerTime.time - currentTime.time
+//        val endDelay = startDelay + 2 * 60 * 1000  // 2 minutes in milliseconds
+//// Schedule the phone to be silenced at the start time
+//        if (startDelay > 0) {
+//            Timer().schedule(timerTask {
+//                audioManager.ringerMode = AudioManager.RINGER_MODE_SILENT
+//                println("Phone silenced at $prayerTime")
+//
+//                // Schedule the phone to return to normal mode at the end time
+//                Timer().schedule(timerTask {
+//                    audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
+//                    println("Phone restored to normal mode 2 minutes after $prayerTime")
+//                }, endDelay)
+//            }, startDelay)
+//        } else {
+//            println("Scheduled prayer time has already passed.")
+//        }
+//    }
 
 }
