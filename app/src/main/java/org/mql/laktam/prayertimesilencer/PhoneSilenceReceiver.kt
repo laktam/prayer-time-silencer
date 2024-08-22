785a5b7a3ef2dbd558ad4706610867f43e210803
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-22 22:59:47 +0100

    add PhoneSilenceReceiver a broadcastReceiver  that responds to the alarm broadcast to silence the phone and create a timer to return it to normal

---
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/PhoneSilenceReceiver.kt
@@ -0,0 +1,30 @@
+package org.mql.laktam.prayertimesilencer
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
+import android.widget.Toast
+import java.util.Timer
+import kotlin.concurrent.timerTask
+/*
+BroadcastReceiver:
+
+A component that responds to broadcast messages from other applications or from the system itself.
+ In this case, PhoneSilenceReceiver responds to an alarm broadcast.
+ */
+class PhoneSilenceReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        Toast.makeText(context, "Alarm received, silencing phone", Toast.LENGTH_SHORT).show()
+        val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        audioManager.ringerMode = AudioManager.RINGER_MODE_SILENT
+
+        println("Phone silenced by AlarmManager")
+
+        // Schedule to turn the ringer mode back to normal after 8 minutes
+        Timer().schedule(timerTask {
+            audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
+            println("Phone restored to normal mode after 8 minutes by AlarmManager")
+        }, 1 * 60 * 1000L)
+    }
+}
