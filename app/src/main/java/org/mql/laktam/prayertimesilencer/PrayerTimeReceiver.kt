3349039609ca5069ec6efb2dce3d2f1e42d6bfd3
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-22 01:10:39 +0100

    add PrayerTimeReceiver a BroadcastReceiver used to relaunch the service each day to refetch and reschedule phone for silencing in prayer times

--- app/src/main/java/org/mql/laktam/prayertimesilencer/PrayerTimeReceiver.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/PrayerTimeReceiver.kt
@@ -1,4 +1,19 @@
 package org.mql.laktam.prayertimesilencer
 
-class PrayerTimeReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.content.ContextCompat
+/*
+When the BroadcastReceiver is triggered, it will start the PrayerTimeService.
+ If the service is already running, this command will essentially "relaunch" it,
+  meaning it will restart the service, allowing it to fetch and schedule new prayer times.
+If the service was stopped or not running for any reason, this will start it again.
+ */
+class PrayerTimeReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        // Start the service to fetch and schedule new prayer times
+        val serviceIntent = Intent(context, PrayerTimeService::class.java)
+        ContextCompat.startForegroundService(context, serviceIntent)
+    }
 }
