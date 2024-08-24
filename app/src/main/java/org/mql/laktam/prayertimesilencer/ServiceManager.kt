622d61ede1f9a47c76d41181d416ac3b487b7cf7
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-25 00:27:08 +0100

    update ServiceManager to use SharedPreferences

--- app/src/main/java/org/mql/laktam/prayertimesilencer/ServiceManager.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/ServiceManager.kt
@@ -1,6 +1,22 @@
 package org.mql.laktam.prayertimesilencer
 
+import android.content.Context
+
+//object ServiceManager {
+//    var isServiceRunning: Boolean = false
+//}
 object ServiceManager {
-    var isServiceRunning: Boolean = false
+    private const val PREFS_NAME = "PrayerTimeServicePrefs"
+    private const val KEY_SERVICE_RUNNING = "service_running"
     var silenceTime = 30  * 60 * 1000L
+
+    fun isServiceRunning(context: Context): Boolean {
+        val sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        return sharedPreferences.getBoolean(KEY_SERVICE_RUNNING, false)
+    }
+
+    fun setServiceRunning(context: Context, isRunning: Boolean) {
+        val sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        sharedPreferences.edit().putBoolean(KEY_SERVICE_RUNNING, isRunning).apply()
+    }
 }
