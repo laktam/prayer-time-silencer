3f7f91b82a06aa50ba03c5039d892fa9965a6ead
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-27 15:57:31 +0100

    add MainViewModel for managing and loading prayer times with silence durations

--- app/src/main/java/org/mql/laktam/prayertimesilencer/MainViewModel.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/MainViewModel.kt
@@ -1,4 +1,36 @@
 package org.mql.laktam.prayertimesilencer
 
-class MainViewModel {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class MainViewModel : ViewModel() {
+    private val _prayerTimes = MutableStateFlow<List<Pair<String, String>>>(emptyList())
+    val prayerTimes: StateFlow<List<Pair<String, String>>> = _prayerTimes.asStateFlow()
+
+    fun loadPrayerTimes(context: Context) {
+        viewModelScope.launch {
+            val sharedPreferences = context.getSharedPreferences("PrayerTimesPreferences", Context.MODE_PRIVATE)
+            val timeStrings = sharedPreferences.getStringSet("scheduledTimes", setOf()) ?: setOf()
+
+            val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+            val silenceDuration = ServiceManager.silenceTime
+
+            val silenceTimes = timeStrings.map { timeString ->
+                val startTime = timeFormat.parse(timeString)
+                val endTime = Date(startTime.time + silenceDuration)
+
+                timeString to timeFormat.format(endTime)
+            }.sortedBy { it.first }
+
+            _prayerTimes.value = silenceTimes
+        }
+    }
 }
