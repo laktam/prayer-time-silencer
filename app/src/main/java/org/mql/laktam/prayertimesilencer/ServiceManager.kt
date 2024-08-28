bac2bbb62c5668a3377db842e5087a93c85696ce
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-28 20:38:21 +0100

    update ServiceManager to initialize StateFlow with the saved state from shared preferences and update the shared preferences whenever the state changes

--- app/src/main/java/org/mql/laktam/prayertimesilencer/ServiceManager.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/ServiceManager.kt
@@ -1,22 +1,47 @@
 package org.mql.laktam.prayertimesilencer
 
 import android.content.Context
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-//object ServiceManager {
-//    var isServiceRunning: Boolean = false
-//}
 object ServiceManager {
     private const val PREFS_NAME = "PrayerTimeServicePrefs"
     private const val KEY_SERVICE_RUNNING = "service_running"
-    var silenceTime = 30  * 60 * 1000L
+    private val _isServiceRunning = MutableStateFlow(false)
+    val isServiceRunning: StateFlow<Boolean> = _isServiceRunning
 
-    fun isServiceRunning(context: Context): Boolean {
+    var silenceTime = 30 * 60 * 1000L
+
+    fun initializeServiceState(context: Context) {
         val sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-        return sharedPreferences.getBoolean(KEY_SERVICE_RUNNING, false)
+        _isServiceRunning.value = sharedPreferences.getBoolean(KEY_SERVICE_RUNNING, false)
     }
 
     fun setServiceRunning(context: Context, isRunning: Boolean) {
+        _isServiceRunning.value = isRunning
         val sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         sharedPreferences.edit().putBoolean(KEY_SERVICE_RUNNING, isRunning).apply()
     }
 }
+//package org.mql.laktam.prayertimesilencer
+//
+//import android.content.Context
+//
+////object ServiceManager {
+////    var isServiceRunning: Boolean = false
+////}
+//object ServiceManager {
+//    private const val PREFS_NAME = "PrayerTimeServicePrefs"
+//    private const val KEY_SERVICE_RUNNING = "service_running"
+//    var silenceTime = 30  * 60 * 1000L
+//
+//    fun isServiceRunning(context: Context): Boolean {
+//        val sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+//        return sharedPreferences.getBoolean(KEY_SERVICE_RUNNING, false)
+//    }
+//
+//    fun setServiceRunning(context: Context, isRunning: Boolean) {
+//        val sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+//        sharedPreferences.edit().putBoolean(KEY_SERVICE_RUNNING, isRunning).apply()
+//    }
+//}
