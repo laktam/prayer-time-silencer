bf8a57fa887f609666cbaeef18863a021feeb116
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-29 16:31:40 +0100

    add colors

--- app/src/main/java/org/mql/laktam/prayertimesilencer/ui/theme/Color.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+ val LightBackgroundColor = Color(0xFFF2F2F2) // Light Gray
+ val CardBackgroundColor = Color(245,245,245) // White
+ val PrayerNameTextColor = Color(63,81,181) //FF2E86C1 Vibrant Blue
+ val TimeTextColor = Color(0xFF333333) // Dark Gray
+ val StopButtonBackgroundColor = Color(0xFFD32F2F) // Bright Red
+ val StopButtonTextColor = Color(0xFFFFFFFF) // Whiteor(0xFF4682B4) // Even darker blue
--- app/src/main/java/org/mql/laktam/prayertimesilencer/ui/theme/Theme.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/ui/theme/Theme.kt
@@ -9,19 +9,38 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+    primary = PrayerNameTextColor,
+    onPrimary = Color.White,
+    secondary = StopButtonBackgroundColor,
+    onSecondary = StopButtonTextColor,
+    background = LightBackgroundColor,
+    onBackground = TimeTextColor,
+    surface = CardBackgroundColor,
+    onSurface = TimeTextColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+//    primary = Purple40,
+//    secondary = PurpleGrey40,
+//    tertiary = Pink40
 
+
+    primary = PrayerNameTextColor,
+    onPrimary = Color.White,
+    secondary = StopButtonBackgroundColor,
+    onSecondary = StopButtonTextColor,
+    background = CardBackgroundColor,
+    primaryContainer = CardBackgroundColor, // Top bar bg
+    onBackground = TimeTextColor,
+    surfaceVariant = CardBackgroundColor,
+    onSurface = TimeTextColor
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
