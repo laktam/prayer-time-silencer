fb7bdc784b98c5f963fa395bdf1a13ab44fb9e17
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-29 20:54:34 +0100

    define new fonts

--- app/src/main/java/org/mql/laktam/prayertimesilencer/ui/theme/Type.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/ui/theme/Type.kt
@@ -2,19 +2,43 @@ package org.mql.laktam.prayertimesilencer.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.mql.laktam.prayertimesilencer.R
 
 // Set of Material typography styles to start with
+val airstrip = FontFamily(
+    Font(R.font.airstrip),
+)
+
+val sparkyStones = FontFamily(
+    Font(R.font.sparkystones),
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily.Cursive,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+displaySmall = TextStyle(
+    fontFamily = airstrip,
+    fontWeight = FontWeight.Normal,
+    fontSize = 28.sp,
+    lineHeight = 24.sp,
+    letterSpacing = 0.5.sp
+),
+    titleLarge = TextStyle(
+        fontFamily = airstrip,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
