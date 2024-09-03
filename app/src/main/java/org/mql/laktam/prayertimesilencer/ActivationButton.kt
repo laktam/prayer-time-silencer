98b3d815e2308d9eea8d43075a9b1600a4eac2b9
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-09-03 03:20:20 +0100

    ask for permissions only when start button is clicked (only if they are not already granted)

--- app/src/main/java/org/mql/laktam/prayertimesilencer/ActivationButton.kt
+++ app/src/main/java/org/mql/laktam/prayertimesilencer/ActivationButton.kt
@@ -1,12 +1,12 @@
 package org.mql.laktam.prayertimesilencer
 
 import android.Manifest
-import android.app.Activity
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.provider.Settings
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -18,8 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.core.app.ActivityCompat
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -35,50 +33,56 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun ActivationFab(viewModel: MainViewModel) {
     val context = LocalContext.current
-
-    // State variables for permissions and service status
-    var locationPermissionGranted by remember { mutableStateOf(false) }
-    var doNotDisturbPermissionGranted by remember { mutableStateOf(false) }
-    val isServiceRunning by viewModel.isServiceRunning.collectAsState()
-
-    // Permission checks and requests
-    LaunchedEffect(Unit) {
-        // Check for location permission
-        if (!locationPermissionGranted) {
-            locationPermissionGranted = ActivityCompat.checkSelfPermission(
-                context, Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-
-            if (!locationPermissionGranted) {
-                ActivityCompat.requestPermissions(
-                    context as Activity,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    1
-                )
-            }
-        }
-
-        // Check for Do Not Disturb permission
-        if (!doNotDisturbPermissionGranted) {
-            val notificationManager =
+    val notificationManager =
                 context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            doNotDisturbPermissionGranted =
-                notificationManager.isNotificationPolicyAccessGranted
 
-            if (!doNotDisturbPermissionGranted) {
-                val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
-                context.startActivity(intent)
+//
+    val locationPermissionsState = remember { mutableStateOf(false) }
+    val isServiceRunning by viewModel.isServiceRunning.collectAsState()
+    val doNotDisturbPermissionGranted = remember { mutableStateOf(notificationManager.isNotificationPolicyAccessGranted()) }
+
+    val locationPermissionLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val locationPermissionsGranted = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true &&
+                    permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+        doNotDisturbPermissionGranted.value =
+        notificationManager.isNotificationPolicyAccessGranted
+
+            if (locationPermissionsGranted && doNotDisturbPermissionGranted.value) {
+                // permissions granted
+                // first button click => start service
+                startService(context)
+                viewModel.setServiceRunning(context, true)
+                println("permissions granted.")
+            } else {
+                // Location permissions denied
+                println("permissions denied.")
             }
+            locationPermissionsState.value = locationPermissionsGranted
         }
-    }
 
     val handleFabClick = {
-        if (isServiceRunning) {
-            stopService(context)
-            viewModel.setServiceRunning(context, false)
-        } else {
-            startService(context)
-            viewModel.setServiceRunning(context, true)
+        if (!doNotDisturbPermissionGranted.value) {
+                val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+                context.startActivity(intent)
+        }
+        if(!locationPermissionsState.value) {
+            locationPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+
+        if (locationPermissionsState.value && doNotDisturbPermissionGranted.value){
+            if (isServiceRunning) {
+                stopService(context)
+                viewModel.setServiceRunning(context, false)
+            } else {
+                startService(context)
+                viewModel.setServiceRunning(context, true)
+            }
         }
     }
 
@@ -114,3 +118,71 @@ fun stopService(context: Context) {
     context.stopService(serviceIntent)
     ServiceManager.setServiceRunning(context, false) // Update service state
 }
+
+//@Composable
+//fun ActivationFab(viewModel: MainViewModel) {
+//    val context = LocalContext.current
+//
+//    // State variables for permissions and service status
+//    var locationPermissionGranted by remember { mutableStateOf(false) }
+//    var doNotDisturbPermissionGranted by remember { mutableStateOf(false) }
+//    val isServiceRunning by viewModel.isServiceRunning.collectAsState()
+//
+//    // Permission checks and requests
+//    LaunchedEffect(Unit) {
+//        // Check for location permission
+//        if (!locationPermissionGranted) {
+//            locationPermissionGranted = ActivityCompat.checkSelfPermission(
+//                context, Manifest.permission.ACCESS_FINE_LOCATION
+//            ) == PackageManager.PERMISSION_GRANTED
+//
+//            if (!locationPermissionGranted) {
+//                ActivityCompat.requestPermissions(
+//                    context as Activity,
+//                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+//                    1
+//                )
+//            }
+//        }
+//
+//        // Check for Do Not Disturb permission
+//        if (!doNotDisturbPermissionGranted) {
+//            val notificationManager =
+//                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//            doNotDisturbPermissionGranted =
+//                notificationManager.isNotificationPolicyAccessGranted
+//
+//            if (!doNotDisturbPermissionGranted) {
+//                val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+//                context.startActivity(intent)
+//            }
+//        }
+//    }
+//
+//    val handleFabClick = {
+//        if (isServiceRunning) {
+//            stopService(context)
+//            viewModel.setServiceRunning(context, false)
+//        } else {
+//            startService(context)
+//            viewModel.setServiceRunning(context, true)
+//        }
+//    }
+//
+//    FloatingActionButton(onClick = { handleFabClick() }) {
+//        Row(
+//            modifier = Modifier.padding(horizontal = 8.dp),
+//            verticalAlignment = Alignment.CenterVertically
+//        ) {
+//            Icon(
+//                imageVector = if (isServiceRunning) Icons.Filled.Close else Icons.Filled.PlayArrow,
+//                contentDescription = if (isServiceRunning) "Stop" else "Start"
+//            )
+//            Text(
+//                text = if (isServiceRunning) stringResource(R.string.stop) else stringResource(R.string.start),
+//                style = MaterialTheme.typography.bodyLarge,
+//                modifier = Modifier.padding(start = 8.dp)
+//            )
+//        }
+//    }
+//}
